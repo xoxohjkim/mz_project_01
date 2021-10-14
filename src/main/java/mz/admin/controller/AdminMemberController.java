0fdbff40e9303fddd0779572d1431ffd5fbd168b
mz <mz@DESKTOP-BPG85LJ>
2021-10-14 10:53:26 +0900

    admin controller

--- src/main/java/mz/admin/controller/AdminMemberController.java
+++
@@ -1,5 +0,0 @@
-package mz.admin.controller;
-
-public class AdminMemberController {
-
-}
---
+++ src/main/java/mz/controller/admin/AdminMemberController.java
@@ -0,0 +1,11 @@
+package mz.controller.admin;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Controller
+public class AdminMemberController {
+
+}
